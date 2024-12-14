58277c02d210ebe2adafb3f3bbb54c01bdb5eed3
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2024-12-14 18:16:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='quickllama',
+    name='ollama-manager',
     version='0.0.1',
     description='Run Ollama models on Colab easily',
     long_description=README,
     long_description_content_type="text/markdown",
     author='Nuhman PK',
-    url='https://github.com/nuhmanpk/quickllama',
-    packages=find_packages(include=['quickllama']),
+    url='https://github.com/nuhmanpk/ollama-manager',
+    packages=find_packages(include=['ollama-manager']),
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
